391b2822b17146a855c9b8ddcbe4607569c587a9
LisetteAntigua <antigua223@gmail.com>
2023-10-23 19:52:53 -0400

    Initial Commit

---
+++ ApplePie_2023/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  ApplePie_2023
+//
+//  Created by Lisette Antigua on 10/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    @IBOutlet weak var treeImageViewLabel: UIImageView!
+    
+    @IBOutlet weak var correctWordLabel: UILabel!
+    
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+    }
+    
+    
+}
+
